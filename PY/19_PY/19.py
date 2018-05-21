c76ffad57479f4d7c66d447536ada7daee6e4344
OB <oliverbraun89@gmail.com>
2018-05-21 17:10:13 +0200

    feat: Annotations 19

--- PY/19_PY/19.py
+++ PY/19_PY/19.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
 import datetime
 
 current = datetime.date(1901,01,01)
