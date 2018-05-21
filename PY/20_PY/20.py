bd82d032b308589de9783ce0c0d707115fc45d8b
OB <oliverbraun89@gmail.com>
2018-05-21 17:11:25 +0200

    feat: Solve 20

---
+++ PY/20_PY/20.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#Find the sum of the digits in the number 100!
+
+def digitsum(n):
+  nn = str(n)
+  return sum([int(x) for x in nn])
+
+def fac(n):
+  if n == 0:
+    return 1
+  else:
+    return n*fac(n-1)
+
+print digitsum(fac(100))
