4a33165d37da598bae46cd6e153c4cedbd57f1a9
OB <oliverbraun89@gmail.com>
2018-05-23 18:23:00 +0200

    feat: Solve 48

---
+++ PY/48_PY/48.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#The series 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+#Find the last ten digits of the series 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+res = 0
+modulus = 10**10
+for n in xrange(1, 1001):
+  res += ( ((n % modulus)**n) % modulus  )
+
+print res % modulus
