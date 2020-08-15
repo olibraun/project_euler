26507fe5667d89ec1db16f597e88686406b11d93
Oliver Braun <oliver.braun@inform-software.com>
2020-08-15 21:07:33 +0200

    fix: python3

--- PY/011.py
+++ PY/011.py
@@ -32,31 +32,31 @@ grid = [[int (n) for n in l] for l in grid]
 max = 0
 
 #Check horizontally
-for i in xrange(0, 20):
-  for j in xrange(0, 20-4):
+for i in range(0, 20):
+  for j in range(0, 20-4):
     prod = grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3]
     if prod > max:
       max = prod
 
 #Check vertically
-for j in xrange(0, 20):
-  for i in xrange(0, 20-4):
+for j in range(0, 20):
+  for i in range(0, 20-4):
     prod = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j]
     if prod > max:
       max = prod
 
 #Check diagonally like this: \
-for i in xrange(0, 20-4):
-  for j in xrange(0, 20-4):
+for i in range(0, 20-4):
+  for j in range(0, 20-4):
     prod = grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3]
     if prod > max:
       max = prod
 
 #Check diagonally like this: /
-for i in xrange(3, 20):
-  for j in xrange(0, 20-4):
+for i in range(3, 20):
+  for j in range(0, 20-4):
     prod = grid[i][j] * grid[i-1][j+1] * grid[i-2][j+2] * grid[i-3][j+3]
     if prod > max:
       max = prod
 
-print max
+print(max)
